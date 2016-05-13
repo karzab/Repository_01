7663b066faea52f5670caaf94c53deadbaee6db7
Riccardo Loffredo <karzab@hotmail.it>
2016-05-14 01:03:17 +0200

    Ristretto il form

--- Robot/Robot/Form1.Designer.cs
+++ Robot/Robot/Form1.Designer.cs
@@ -56,7 +56,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(462, 265);
+            this.ClientSize = new System.Drawing.Size(410, 314);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
