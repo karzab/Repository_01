51a453d2739468bb02a949453937eff7750d7fcd
Riccardo Loffredo <karzab@hotmail.it>
2016-05-14 01:45:02 +0200

    Titolo sul form

--- Robot/Robot/Form1.Designer.cs
+++ Robot/Robot/Form1.Designer.cs
@@ -60,7 +60,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Test";
             this.ResumeLayout(false);
             this.PerformLayout();
 
