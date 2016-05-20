ce401159c1e01ce92d6c121a68ac9fee36312627
Riccardo Loffredo <karzab@hotmail.it>
2016-05-21 01:39:22 +0200

    Mergiato i cambiamenti

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -10,13 +10,12 @@ namespace Test
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
             Console.WriteLine("Sono Riccardo_2!");
-=======
+
 
             Console.WriteLine("Ciao Mondo!");
             Console.WriteLine("Ciao Luna!");
->>>>>>> 3154f556481832db077fa86ea7b38eb8698b89c6
+
             Console.ReadLine();
         }
     }
