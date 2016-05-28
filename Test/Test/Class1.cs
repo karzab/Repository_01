dc248b1157b86567d102c8606d49c5aacc4ec4ce
Riccardo Loffredo <karzab@hotmail.it>
2016-05-28 16:40:10 +0200

    Aggiunto un file

---
+++ Test/Test/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Class1
+    {
+        int i;
+        public Class1() {
+            i = 0;
+        }
+    }
+}
